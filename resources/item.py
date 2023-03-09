16e894cce9b6d01862a779419b45840c5101dd63
KonuTech <borowiec.k@gmail.com>
2023-03-09 14:12:30 +0100

    yet another change

--- resources/item.py
+++ resources/item.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 
 blp = Blueprint("Items", __name__, description="Operations on items")
@@ -11,6 +12,7 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -24,10 +26,9 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad request.Ensure 'price', and 'name' are included in the JSON payload.")
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data  # updates dictionary by another dictionary
@@ -40,20 +41,14 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
         # return "Hello, world!"
-        return {"items": list(items.values())}
+        return items.values()
 
-    def post(self):
-        item_data = request.get_json()
-
-        if (
-                "price" not in item_data
-                or "store_id" not in item_data
-                or "name" not in item_data
-        ):
-            abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
-
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                     item_data["name"] == item["name"]
