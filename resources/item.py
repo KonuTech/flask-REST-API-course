623013aee9f86291ed493fec44b7c0b64056bc40
KonuTech <borowiec.k@gmail.com>
2023-03-09 19:35:34 +0100

    yet another change

--- resources/item.py
+++ resources/item.py
@@ -16,29 +16,27 @@ blp = Blueprint("Items", "items", description="Operations on items")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented.")
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-            item |= item_data  # updates dictionary by another dictionary
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
 
-            return item
+        db.session.add(item)
+        db.session.commit()
 
-        except KeyError:
-            abort(404, message="Item not found")
+        raise item
 
 
 @blp.route("/item")
