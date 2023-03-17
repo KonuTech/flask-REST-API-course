c3d6afaf633139c324ea028e26496b6c14ce1994
KonuTech <borowiec.k@gmail.com>
2023-03-17 17:47:04 +0100

    yet another change

--- tasks.py
+++ tasks.py
@@ -1,12 +1,20 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
         f"https://api.mailgun.net/v3/{DOMAIN}/messages",
         auth=("api", os.getenv("MAILGUN_API_KEY")),
@@ -14,7 +22,8 @@ def send_simple_message(to, subject, body):
             "from": f"Konrad Borowiec <mailgun@{DOMAIN}>",
             "to": [to],
             "subject": subject,
-            "text": body
+            "text": body,
+            "html": html
         }
     )
 
@@ -23,5 +32,6 @@ def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "Successfully signed up",
-        f"Hi {username}! You have successfully signed up to the Stores REST API."
+        f"Hi {username}! You have successfully signed up to the Stores REST API.",
+        render_template("email/action.html", username=username)
     )
